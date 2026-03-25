fd2c7c6a26faf28a1415bd1cdc84526df60ac063
karakatzaslan <karakatzaslan@gmail.com>
2026-03-25 23:19:51 +0500

    Clean project structure

--- my-app/src/app/data/categories.ts
+++
@@ -1,8 +0,0 @@
-import { Category } from '../models/category.model';
-
-export const categories: Category[] = [
-  { id: 1, name: 'Smartphones' },
-  { id: 2, name: 'Laptops' },
-  { id: 3, name: 'Headphones' },
-  { id: 4, name: 'Tablets' },
-];
